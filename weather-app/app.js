3ab11dc40af91f449e576fd03913c1861e90d8b2
unknown <athifahamedj@gmail.com>
2023-05-02 15:40:34 +0530

    added JSON weather API

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,3 +1,14 @@
-console.log("starting");
+const request = require("request");
 
-console.log("stopping");
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=London&appid=6fb944ff2c125a9135bad967a9e20a1a";
+
+const data = request({ url: url }, (error, response) => {
+  const result = JSON.parse(response.body);
+
+  try {
+    console.log(result.weather);
+  } catch {
+    console.log(error);
+  }
+});
