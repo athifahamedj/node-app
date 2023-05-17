51683c2314df4bbd149cc972d0e655aa06ba5135
athif <athifahamedj@gmail.com>
2023-05-17 18:27:33 +0530

    added files

--- node-scratch/web-server/app.js
+++ node-scratch/web-server/app.js
@@ -1,16 +1,43 @@
 const path = require("path");
+const hbs = require("hbs");
 const express = require("express");
 
 // console.log(path.join(__dirname, "/public"));
 
 const app = express();
-const publicPath = path.join(__dirname + "/public");
+const publicPath = path.join(__dirname, "/public");
+const viewpath = path.join(__dirname, "./templates/views");
+
+const partialPath = path.join(__dirname, "./templates/partials");
+
+hbs.registerPartials(partialPath);
+
+app.set("view engine", "hbs");
+app.set("views", viewpath);
 app.use(express.static(publicPath));
 
 // console.log(__dirname);
-// app.get("", (req, res) => {
-//   res.send("<h1>Express JS running up</h1>");
-// });
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Index",
+    name: "Athif",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Athif",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Athif",
+    message: "This page helps you with whatever you need to know!!",
+  });
+});
 
 app.get("/home", (req, res) => {
   res.send("This is HOMEPAGE");
@@ -18,6 +45,14 @@ app.get("/home", (req, res) => {
 
 app.get("/weather", (req, res) => {
   res.send("This is Weather Application");
+});
+
+app.get("*", (req, res) => {
+  res.send("No page found");
+});
+
+app.get("/help/*", (req, res) => {
+  res.send("No help article found");
 });
 
 app.listen(3001, () => {
